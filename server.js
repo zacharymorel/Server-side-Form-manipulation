05116bafb64db27dadcb7d9a7a853d078d337b4b
Zachary Morel <morelz133@gmail.com>
2017-07-26 15:52:36 -0400

    the form now transfers the data to the page it needs to.

--- server.js
+++ server.js
@@ -1,26 +1,25 @@
 const express = require('express')
 const mustacheExpress = require('mustache-express')
+const bodyParser = require('body-parser')
 
 const app = express()
 
 app.engine('mst', mustacheExpress())
 app.set('views', './views')
-app.set('viewengine', 'mst' )
+app.set('view engine', 'mst' )
 
-app.get('/', () => {
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
+app.get('/', (request, response) => {
   response.render('home')
 })
 
-
-
-
-
-
-
-
-
-
+app.post('/signup', (request, response) => {
+  response.render('signup',request.body)
+})
 
 app.listen(3000, () => {
-  console.log('my the force by with you');
+  console.log('may the force by with you');
 })
